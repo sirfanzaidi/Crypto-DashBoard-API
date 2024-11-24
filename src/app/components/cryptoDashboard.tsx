550f1ae833dd37ad1cd26f6dea2675b6f7919dc8
Irfan Hussain <I.hussain@expressmedia.int>
2024-11-24 16:27:08 +0500

    creating a stunning and responsive dashboard using React and Tailwind CSS, paired with seamless API integration for real-time cryptocurrency data! 📈��

---
+++ src/app/components/cryptoDashboard.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const CryptoDashboard = () => {
+  const [coins, setCoins] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchCoinsRates = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd`,
+          {
+            headers: {
+              Authorization: `Bearer v2VnuaTz6KuLxnPKM3xLNFlTP3dkHQRt`,
+            },
+          }
+        );
+        setCoins(response.data);
+      } catch (error) {
+        console.error("error fetching coin data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCoinsRates();
+  }, []);
+  if (loading) {
+    return (
+      <div className="text-ecnter text-xl text-red-800">Loading Data.... </div>
+    );
+  }
+  return (
+    <div className="max-w-7 mx-auto p-4">
+      <h1 className="text-4xl font-semibold text-center text-black mb-8">
+        Crypto Currency Deshboard
+      </h1>
+      <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {coins.map((coins) => (
+          <div
+            key={coins.id}
+            className="bg-white p-6 rounded-lg shadow-xl transform transi-300 hover:scale-105 hover:shadow-2xl"
+          >
+            <h2 className="text-xl font-bold text-center text-red-600 mb-4 uppercase">
+              {coins.name}
+            </h2>
+            <div className="mb-4">
+              <p className="text-lg text-blue-600"> Price (USD):</p>
+              <p> ${coins.current_price.toFixed(2)}</p>
+            </div>
+            <div className="mb-4">
+              <p className="text-lg text-slate-600">Market Cap:</p>
+              <p className="text-xl font-bold text-slate-800"></p>
+            </div>
+            <div>
+              <p className="text-lg text-blue-600 ">24h Change: </p>
+              <p
+                className={`text-xl font-semiold ${
+                  coins.price_change_percentage_24 < 0
+                    ? "text-red-500"
+                    : "text-gray-500"
+                } `}
+              >
+                {coins.price_change_Percentage_24.toFixed(2)}%
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CryptoDashboard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,194 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import CryptoDashboard from "./components/cryptoDashboard";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [showCrypto, setShowCrypto] = useState(false);
+  const handleExploreClick = () => {
+    setShowCrypto(true);
+    document
+      .getElementById("crypto-section")
+      ?.scrollIntoView({ behavior: "smooth" });
+  };
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <div className=" bg-black h-screen">
+      <header className="text-center p-16 px-4 text-white">
+        <h1 className="text-6xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-rose-400 to bg-indigo-400">
+          Crypto Currency Data Dashboard
+          <br />
+          your gateway to comprehensive Crypto Insights!
+        </h1>
+        <br />
+        <p className="text-xl bm-8 max-w-2xl mx-auto">
+          stay updated with live crypto prices in market. easily acces the
+          market coin rates with API
+        </p>
+        <button
+          onClick={handleExploreClick}
+          className="inline-block px-10 py-5 bg-yellow-500 text-black font-semibold rounded-full shadow-lg  transition-transform hover:bg-cyan-400 hover:scale-105 "
+          aria-label="Explore Cryptocurrency Stats"
+        >
+          Cryptocurrency Current Data Stats
+        </button>
+      </header>
+
+      <section className="text-gray-400 bg-black body-font">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          <div className="flex flex-col text-center w-full mb-20">
+            <h2 className="text-xs text-indigo-400 tracking-widest font-medium title-font mb-1 uppercase">
+              Explore our feature section
+            </h2>
+            <h1 className="sm:text-3xl text-2xl font-medium title-font text-white">
+              Crypto World of irfan
+            </h1>
+          </div>
+          <div className="flex flex-wrap -m-4">
+            <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full bg-gray-800 bg-opacity-60 p-8 flex-col">
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                    </svg>
+                  </div>
+                  <h2 className="text-white text-lg title-font font-medium">
+                  feature heading 1
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <p className="leading-relaxed text-base">
+                    Blue bottle crucifix vinyl post-ironic four dollar toast
+                    vegan taxidermy. Gastropub indxgo juice poutine.
+                  </p>
+                  <a className="mt-3 text-indigo-400 inline-flex items-center">
+                    Read More
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full bg-gray-800 bg-opacity-60 p-8 flex-col">
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                      <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                  </div>
+                  <h2 className="text-white text-lg title-font font-medium">
+                  feature heading 2
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <p className="leading-relaxed text-base">
+                    Blue bottle crucifix vinyl post-ironic four dollar toast
+                    vegan taxidermy. Gastropub indxgo juice poutine.
+                  </p>
+                  <a className="mt-3 text-indigo-400 inline-flex items-center">
+                    Read More
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/3">
+              <div className="flex rounded-lg h-full bg-gray-800 bg-opacity-60 p-8 flex-col">
+                <div className="flex items-center mb-3">
+                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle cx="6" cy="6" r="3"></circle>
+                      <circle cx="6" cy="18" r="3"></circle>
+                      <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+                    </svg>
+                  </div>
+                  <h2 className="text-white text-lg title-font font-medium">
+                  feature heading 3
+                  </h2>
+                </div>
+                <div className="flex-grow">
+                  <p className="leading-relaxed text-base">
+                    Blue bottle crucifix vinyl post-ironic four dollar toast
+                    vegan taxidermy. Gastropub indxgo juice poutine.
+                  </p>
+                   <a className="mt-3 text-indigo-400 inline-flex items-center">
+                    Read More
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      </section>
+
+      {showCrypto && (
+        <section id="crypto-section" className="py-16 bg-white">
+          <div className="max-w-7xl mx-auto px-6">
+            <CryptoDashboard />
+          </div>
+        </section>
+      )}
+      <footer className="bg-black text-cyan-400 py-6 text-center">
+        <p>
+          &copy; 2024 Syed Irfan Hussain Zaidi Crypto Currency Dashboard. All
+          Rights Reserved.
+        </p>
       </footer>
     </div>
   );
